bd36cf68bf3b5e0ace6c3a73c8feef5183083c68
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-09-12 21:50:14 +0300

    write pseudo code for 2 binaries that are built

---
+++ src/passive.cpp
@@ -0,0 +1,10 @@
+// main for the pasive app that is called on the client 
+// main() {
+    // is started when ssh -R has been opened by the remote
+    // open `ssh -L 7778:127.0.0.1:7777 SERVER_USER@SERVER`. in the future, the port could be in the received packet
+    // start listening / reding /tmp/janisnvim.sock
+    // run `nvim --server /tmp/janisnvim.sock --remote-ui`
+    // connect(127.0.0.1:7778)
+    // read nvim events from /tmp/janisnvim.sock, process them and forward to the remote headless nvim server
+    // receive events from nvim server and push them to the local nvim remote ui
+// }
---
+++ src/remote.cpp
@@ -0,0 +1,6 @@
+// main for the server app
+// main() {
+    // exec `nvim --listen 127.0.0.1:7777 --headless`
+    // open ssh -R CLIENT_USER@CLIENT
+    // send a packet that gives info about the current dir and whatever esle may be needed 
+// }
